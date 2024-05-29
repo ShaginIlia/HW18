939fa7bfb91808831d674a7acf6d7c98822e20d7
ShaginIlia <166448106+ShaginIlia@users.noreply.github.com>
2024-05-29 21:35:03 +0300

    Add files via upload

--- HW18.py
+++ HW18.py
@@ -4,12 +4,17 @@ class Building:
         self.buildingType = str(type)
 
     def __eq__(self, other):
-        return self.numberOfFloors == self.buildingType
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType ==  other.buildingType
+
 
     def __str__(self):
         return f'{self.numberOfFloors, self.buildingType}'
 
 
 b = Building(2, 'деревянный')
+ab = Building(5, 'stone')
 print(b)
-print(b.numberOfFloors == b.buildingType)
+print(b.numberOfFloors == ab.numberOfFloors)
+print(b.buildingType == ab.buildingType)
+
+
